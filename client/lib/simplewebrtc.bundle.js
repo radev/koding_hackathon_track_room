eafd9122a386ce3b8666140a1dc9a5b47be69fb6
esergion <esergion@gmail.com>
2014-12-08 11:20:21 +0500

    try out our turn server

--- client/lib/simplewebrtc.bundle.js
+++ client/lib/simplewebrtc.bundle.js
@@ -14,7 +14,7 @@ function SimpleWebRTC(opts) {
     var config = this.config = {
             url: 'https://signaling.simplewebrtc.com',
             socketio: {/* 'force new connection':true*/},
-            debug: false,
+            debug: true,
             localVideoEl: '',
             remoteVideosEl: '',
             enableDataChannels: true,
@@ -4904,39 +4904,11 @@ function WebRTC(opts) {
             debug: true,
             // makes the entire PC config overridable
             peerConnectionConfig: {
-                iceServers: [{"url": "stun:stun.l.google.com:19302"},{"url":'stun:stun01.sipphone.com'},
-                                {"url":'stun:stun.ekiga.net'},
-                                {"url":'stun:stun.fwdnet.net'},
-                                {"url":'stun:stun.ideasip.com'},
-                                {"url":'stun:stun.iptel.org'},
-                                {"url":'stun:stun.rixtelecom.se'},
-                                {"url":'stun:stun.schlund.de'},
-                                {"url":'stun:stun.l.google.com:19302'},
-                                {"url":'stun:stun1.l.google.com:19302'},
-                                {"url":'stun:stun2.l.google.com:19302'},
-                                {"url":'stun:stun3.l.google.com:19302'},
-                                {"url":'stun:stun4.l.google.com:19302'},
-                                {"url":'stun:stunserver.org'},
-                                {"url":'stun:stun.softjoys.com'},
-                                {"url":'stun:stun.voiparound.com'},
-                                {"url":'stun:stun.voipbuster.com'},
-                                {"url":'stun:stun.voipstunt.com'},
-                                {"url":'stun:stun.voxgratia.org'},
-                                {"url":'stun:stun.xten.com'},
+                iceServers: [
                                 {
-                                    "url": 'turn:numb.viagenie.ca',
-                                    "credential": 'muazkh',
-                                    "username": 'webrtc@live.com'
-                                },
-                                {
-                                    "url": 'turn:192.158.29.39:3478?transport=udp',
-                                    "credential": 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                                    "username": '28224511:1379330808'
-                                },
-                                {
-                                    "url": 'turn:192.158.29.39:3478?transport=tcp',
-                                    "credential": 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                                    "username": '28224511:1379330808'
+                                    "url": 'turn:146.185.156.70:3478',
+                                    "credential": "justfortestthisfeature",
+                                    "username": "trackroom"
                                 }
                             ]
             },
